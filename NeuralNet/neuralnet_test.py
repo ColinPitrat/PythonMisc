d88ad3041de7aa278fdc4592598877098a31949e
Colin Pitrat <colin.pitrat@gmail.com>
2018-11-21 19:18:28 +0000

    Start working on neural network in python

---
+++ NeuralNet/neuralnet_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-"
+
+import unittest
+import neuralnet
+
+class TestNeuralNet(unittest.TestCase):
+
+    pass
+
+if __name__ == '__main__':
+   unittest.main()
