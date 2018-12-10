608a2eeef6098d8f1765ed32951965a6f87ef10e
Colin Pitrat <colin.pitrat@gmail.com>
2018-12-10 17:05:59 +0000

    Sanity cheeck on tests files

--- NeuralNet/run_tests.sh
+++ NeuralNet/run_tests.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
+
 nb_tests=`ls tests/test*.txt | wc -l`
+
+# Check consistency of all network files with their tests files
 for i in `seq 1 $nb_tests`
 do
+  test_file="tests/test${i}.txt"
+  network_file=`head -n 1 "$test_file"`
+  expected="tests/network${i}.txt"
+  if [ "$network_file" -ne "$expected" ]
+  then
+    echo "ERROR: Expected '$expected' network file in '$test_file', got '$network_file'"
+    exit 1
+  fi
+done
+
+for i in `seq 1 $nb_tests`
+do
+  test_file="tests/test${i}.txt"
+  result_file="tests/result${i}.txt"
   echo " ====== Running test $i ====="
-  if [ -f tests/result${i}.txt ]
+  if [ -f "$result_file" ]
   then
     echo " * Skipping - already ran * "
   else
-    unbuffer python main.py train tests/test${i}.txt | tee tests/result${i}.txt
+    unbuffer python main.py train "$test_file" | tee "$result_file"
   fi
 done
 
